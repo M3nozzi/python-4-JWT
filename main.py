22dba7da4f3dc96c31647ce1b7d0e40580abf691
Fabio Menozzi <f.menozzi@yahoo.com>
2020-07-16 12:39:16 -0300

    Done

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import jwt
 
 
 def create_token(data, secret):
-    token = jwt.encode(data, secret, algorithm='HS256')
+    token = jwt.encode(data, secret,'HS256')
     return token
 
 
 def verify_signature(token):
     try:
-        signature = jwt.decode(token, "acelera", algorithms='HS256')
+        signature = jwt.decode(token, "acelera")
         return signature
     except:
         return({"error": 2})
